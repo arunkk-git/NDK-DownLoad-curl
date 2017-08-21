5300d555e2db44c47f79ab3641dff559a62f3bbb
ARUN KULKARNI <arun.k@altiux.com>
2017-08-21 10:57:03 +0530

    progressbar curl

---
+++ README.md
@@ -0,0 +1,9 @@
+# Using Curl library in Android NDK#
+
+
+Android Sample app to use libcurl with Android NDK
+
+* To build curl library for Android NDK use [Android Curl Builder](https://github.com/bertrandmartel/android-curl-builder) (works on Linux & MAC OS)
+
+* curl / openssl / libz libs included in this repo are built for arm architecture with android platform 19
+
---
+++ app/src/main/obj/local/armeabi-v7a/objs/curl-ndk/curl-http.o.d
@@ -0,0 +1,32 @@
+D:/curl/ndk-curl-sample-master/app/src/main/obj/local/armeabi-v7a/objs/curl-ndk/curl-http.o: \
+  D:/curl/ndk-curl-sample-master/app/src/main/jni/curl-http.cpp \
+  D:/Android/NDK/android-ndk-r14b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\sstream \
+  D:/Android/NDK/android-ndk-r14b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\iostream \
+  D:/curl/ndk-curl-sample-master/app/src/main/jni/../prebuild/include\curl/curl.h \
+  D:/curl/ndk-curl-sample-master/app/src/main/jni/../prebuild/include\curl/curlver.h \
+  D:/curl/ndk-curl-sample-master/app/src/main/jni/../prebuild/include\curl/curlbuild.h \
+  D:/curl/ndk-curl-sample-master/app/src/main/jni/../prebuild/include\curl/curlrules.h \
+  D:/curl/ndk-curl-sample-master/app/src/main/jni/../prebuild/include\curl/easy.h \
+  D:/curl/ndk-curl-sample-master/app/src/main/jni/../prebuild/include\curl/multi.h \
+  D:/curl/ndk-curl-sample-master/app/src/main/jni\Utils.h \
+  D:/Android/NDK/android-ndk-r14b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\fstream
+
+D:/Android/NDK/android-ndk-r14b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\sstream:
+
+D:/Android/NDK/android-ndk-r14b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\iostream:
+
+D:/curl/ndk-curl-sample-master/app/src/main/jni/../prebuild/include\curl/curl.h:
+
+D:/curl/ndk-curl-sample-master/app/src/main/jni/../prebuild/include\curl/curlver.h:
+
+D:/curl/ndk-curl-sample-master/app/src/main/jni/../prebuild/include\curl/curlbuild.h:
+
+D:/curl/ndk-curl-sample-master/app/src/main/jni/../prebuild/include\curl/curlrules.h:
+
+D:/curl/ndk-curl-sample-master/app/src/main/jni/../prebuild/include\curl/easy.h:
+
+D:/curl/ndk-curl-sample-master/app/src/main/jni/../prebuild/include\curl/multi.h:
+
+D:/curl/ndk-curl-sample-master/app/src/main/jni\Utils.h:
+
+D:/Android/NDK/android-ndk-r14b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include\fstream:
